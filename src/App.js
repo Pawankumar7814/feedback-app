a1d3826c18dbf4809897537b16609f20fca51b8a
Pawankumar7814 <pawank4285@gmail.com>
2024-03-06 23:09:08 +0530

    commit feedback App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+// function App(){
+//     return React.createElement('div',{className:"container"},
+//     React.createElement('h1',{},'My App')
+//     )
+// }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const title = 'Blog Post';
+    const body = "This is my blog post";
+    // const comment = [
+    //     { id: 1, text: 'Comment One' },
+    //     { id: 2, text: 'Comment two' },
+    //     { id: 3, text: 'Comment three' },
+    //     { id: 4, text: 'Comment four' }
+    // ];
+
+//     const loading = false;
+//     const showComments = true;
+//     const commentBlock= (<div className='comment'>
+//     Comments ({comment.length})
+//     <ul>
+//         {comment.map((comment, index) => (
+//             <li key={index}>{comment.text}</li>
+//         ))}
+//     </ul>
+// </div>);
+
+    // if (loading) {
+    //     return <h1> Loading...</h1>
+    // }
+
+    return (
+        <>
+            <h1 className="container">{title}</h1>
+            <p>{body} This is how we display the data of variable</p>
+            {/* {showComments && commentBlock} */}
+        </>
+    );
 }
 
 export default App;
